c21e145ab636852cbecc815c1bd5059d523e980f
BenSuzarte <ben.dantassuzarte@gmail.com>
2024-07-08 12:16:36 -0300

    chore: add mercadopago module and service, add order controller servicer and module

---
+++ src/app.module.ts
@@ -0,0 +1,9 @@
+import { Module } from "@nestjs/common";
+import { MercadopagoModule } from "./mercadopago/mercadopago.module";
+
+@Module({
+  imports: [
+    MercadopagoModule
+  ],
+})
+export class AppModule {}
